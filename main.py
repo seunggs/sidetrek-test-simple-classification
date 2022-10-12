08c354ec15b97208d6cdd6b12d6759b11eaceecf
Taeef Najib <taeefnajib@gmail.com>
2022-10-12 20:22:08 +0600

    tuple has been typed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 import sklearn.metrics as metrics
 import pandas as pd
 import numpy as np
+from typing import Tuple
 
 # Collecting data for classification model
 def collect_data(n_samples: int = 1000,
@@ -25,37 +26,43 @@ def collect_data(n_samples: int = 1000,
                                         "feature4"]), np.ravel(pd.DataFrame(ds[1],
                                         columns=["target"]), order='C')
 
+X, y = collect_data()
 
 # Splitting X and y into train and test datasets
-def split_train_test(feature: pd.DataFrame = collect_data()[0],
-                     target: pd.DataFrame = collect_data()[1],
+def split_train_test(feature: pd.DataFrame = X,
+                     target: pd.DataFrame = y,
                      test_size: float = 0.33,
-                     random_state: int = 42) -> pd.DataFrame:
+                     random_state: int = 42) -> Tuple[pd.DataFrame]:
     return train_test_split(feature,
                             target,
                             test_size = test_size,
                             random_state = random_state)
 
+X_train, X_test, y_train, y_test = split_train_test()
+
 # Creating the Multiple Linear Regression model
 def build_model(model: RandomForestClassifier = RandomForestClassifier(),
-                X_train: pd.DataFrame = split_train_test()[0],
-                y_train: pd.DataFrame = split_train_test()[2]) -> RandomForestClassifier:
+                X_train: pd.DataFrame = X_train,
+                y_train: pd.DataFrame = y_train) -> RandomForestClassifier:
     return model.fit(X_train, y_train)
 
 
 # Predicting output using model
 def predict_data(model: RandomForestClassifier = build_model(),
-                 X_test: pd.DataFrame = split_train_test()[1]) -> np.ndarray:
+                 X_test: pd.DataFrame = X_test) -> np.ndarray:
     return model.predict(X_test)
 
+y_pred = predict_data()
 
 # Evaluating model on test data
-def evaluate_model(y_test: pd.DataFrame = split_train_test()[3],
-                   y_pred: np.ndarray = predict_data()) -> tuple:
+def evaluate_model(y_test: pd.DataFrame = y_test,
+                   y_pred: np.ndarray = y_pred) -> Tuple[float]:
     return metrics.accuracy_score(y_test, y_pred), metrics.f1_score(y_test, y_pred), metrics.precision_score(y_test, y_pred), metrics.recall_score(y_test, y_pred), metrics.jaccard_score(y_test, y_pred)
 
-print("Accuracy:",evaluate_model()[0])
-print("F1:",evaluate_model()[1])
-print("Precision:",evaluate_model()[2])
-print("Recall:",evaluate_model()[3])
-print("Jaccard Score",evaluate_model()[4])
+acc, f1, prec, rec, js = evaluate_model()
+
+print("Accuracy:",acc)
+print("F1:",f1)
+print("Precision:",prec)
+print("Recall:",rec)
+print("Jaccard Score:",js)
